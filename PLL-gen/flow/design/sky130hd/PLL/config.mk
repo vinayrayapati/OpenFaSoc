a1627b6a1fa5c2f4668c99b4e452402d8e926ac4
vinayrayapati <vinayrayapati@outlook.com>
2022-12-06 23:28:21 +0530

    final commit

--- PLL-gen/flow/design/sky130hd/PLL/config.mk
+++ PLL-gen/flow/design/sky130hd/PLL/config.mk
@@ -7,11 +7,11 @@ export VERILOG_FILES 		= $(sort $(wildcard ./design/src/$(DESIGN_NICKNAME)/*.v))
 			  	  ../blocks/$(PLATFORM)/PLL.blackbox.v
 export SDC_FILE    		= ./design/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
-export DIE_AREA   	 	= 0 0 250 250
-export CORE_AREA   		= 15 15 230 230
+export DIE_AREA   	 	= 0 0 200 200
+export CORE_AREA   		= 15 15 180 180
 
 # area of the smaller voltage domain
-export VD1_AREA                 = 50 50 150 200
+export VD1_AREA                 = 50 50 100 150
 
 # power delivery network config file
 export PDN_TCL 			= ../blocks/$(PLATFORM)/pdn.tcl
@@ -27,7 +27,7 @@ export DOMAIN_INSTS_LIST 	= ../blocks/$(PLATFORM)/pll_domain_insts.txt
 
 # configuration for placement
 #export MACRO_PLACE_HALO         = 1 1
-#export MACRO_PLACE_CHANNEL      = 20 20
+#export MACRO_PLACE_CHANNEL      = 30 30
 #export MACRO_PLACEMENT          = ../blocks/$(PLATFORM)/manual_macro.tcl
 
 #export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 1
@@ -53,4 +53,4 @@ export PRE_GLOBAL_ROUTE = $(SCRIPTS_DIR)/openfasoc/pre_global_route.tcl
 export CUSTOM_CONNECTION 	= ../blocks/$(PLATFORM)/pll_custom_net.txt
 
 # indicates with how many connections the VIN route from the HEADER cells connects to the VIN power ring
-export VIN_ROUTE_CONNECTION_POINTS = 1
+export VIN_ROUTE_CONNECTION_POINTS = 2
